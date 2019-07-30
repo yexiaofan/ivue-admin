d614cd070a8ce590038563fec222dfdc33925497
yepengfei <yepengfei@ctsi.com.cn>
2019-07-30 17:06:14 +0800

    添加新的404页面

---
+++ src/views/error-pages/404.vue
@@ -0,0 +1,30 @@
+<style lang="less" scoped>
+.page-404 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-of-type {
+    font-size: 184px;
+    font-weight: bold;
+    text-shadow: 5px 1px 5px rgba(0, 0, 0, 1)
+  }
+  span:last-of-type {
+    font-size: 32px;
+  }
+  a {
+    font-size: 20px;
+  }
+}
+</style>
+
+<template>
+  <div class="wrapper page-404">
+    <span>404</span>
+    <span>您访问的页面暂不在地球服务区内！o(ㄒoㄒ)o</span><br>
+    <a href="javascript:void(0)" @click="$router.push('/home')">返回首页</a>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
